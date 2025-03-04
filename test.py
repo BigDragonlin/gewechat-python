021175232716daeb08f6ba1ebdc7fe14673b881a
company <1363992060@qq.com>
2025-03-04 03:24:47 +0000

    add docker compose

--- test.py
+++
@@ -1,7 +0,0 @@
- {
-            "name": "deepseek-ai/DeepSeek-V3",
-            "requester":"siliconflow-chat-completions",
-            "token_mgr": "siliconflow",
-            "tool_call_supported": false,
-            "vision_supported": false
-},
